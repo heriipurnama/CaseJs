4cd92c5b87d7a48fa786d51953030993102742ca
heriipurnama <herii.sinatra@gmail.com>
2021-01-21 01:06:03 +0700

    update

--- Cart.js
+++ Cart.js
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
-</head>
-<body>
-   
-   
-   <script type="application/javascript">
-      `use strict`
+    `use strict`
 
       class Cart {
          constructor() {
@@ -52,7 +40,3 @@
          .setTopSpeed(250)
          .setFeulCapacity("600 Litres")
          .displayCarProps()
-   </script>
-
-</body>
-</html>
--- Log.js
+++ Log.js
@@ -1,1 +1,39 @@
 `use strict`
+
+const data = {
+    info : "This is an information about something.",
+    error : "We can't divide any numbers by zero.",
+    notice : "Someone loves your status.",
+    warning : "Insufficient funds.",
+    debug : "This is debug message.",
+    alert : "Achtung! Achtung!.",
+    critical :"Medic!! We've got critical damages.",
+    emergency : "System hung. Contact system administrator immediately!"
+};
+
+class Log {
+    
+    writeLog() {  
+        
+        for (let i = 0; i < Object.keys(data).length; i++) {
+            
+            let info  = `${Date()} ${data.info}`;
+            let error = `${Date()} ${data.error}`;
+            let notice  = `${Date()} ${data.notice}`;
+            let warning = `${Date()} ${data.warning}`;
+
+            let debug  = `${Date()} ${data.debug}`;
+            let alert  = `${Date()} ${data.alert}`;
+            let critical  = `${Date()} ${data.critical}`;
+            let emergency = `${Date()} ${data.emergency}`;
+
+            return [info, error, notice, warning, debug, alert, critical, emergency]  
+        }
+    }
+}
+
+let log = new Log();
+
+const result = log.writeLog();
+console.log( result);
+
