cf630fc05a59c584292fa16618f994e071ee9c26
heripurnama <heri.purnama@xapiens.id>
2021-01-22 09:36:30 +0700

    update post

--- TypiCode.js
+++ TypiCode.js
@@ -6,7 +6,6 @@ const urlAPI = 'https://jsonplaceholder.typicode.com';
 async function getData(){
     const resPosts = await axios(`${urlAPI}/posts`);
     const resUsers = await axios(`${urlAPI}/users`);
-  
 
     // console.log("post", resPosts.data);
     // console.log("user", resUsers.data);
@@ -14,6 +13,19 @@ async function getData(){
     const a2 = resUsers.data;
 
     console.log('data', a1);
- }
 
- getData();
+    const mapToUser = (post, users) => {
+        const user = users.find((e) => e.id === post.userId);
+        post.user = user;
+    };
+
+    const getAll = async () => {
+        let post = await resPosts;
+        let user = await resUsers;
+        post = post.map((e) => mapToUser(e, user));
+
+    }
+}
+ 
+ 
+getData();
