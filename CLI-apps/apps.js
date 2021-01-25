97d58a0a3e5b2addaf8ee7acec95a494a875334a
heripurnama <heri.purnama@xapiens.id>
2021-01-25 17:13:01 +0700

    update answer

--- CLI-apps/apps.js
+++ CLI-apps/apps.js
@@ -272,16 +272,55 @@ program
           );
         })
 
-    .command("random")
+    .command("randomw")
         .option("--test", "testing", {
           default: 32,
           validator: program.NUMBER,
         })
+        .option("--letters", "ignore string", {
+            default: 32,
+            validator: program.NUMBER,
+          })
         .action(({ args, options }) => {
           console.log(options.test);
           // console.log(args, options);
-        });
-       
+        })
+        
+    .command("random")
+        .option("--panjang <number>", "", {
+          default: 32,
+          validator: program.NUMBER,
+        })
+        .option("--letters <boolean>", "", {
+          default: true,
+        })
+        .option("--numbers <boolean>", "", {
+          default: true,
+        })
+        .option("--uppercase <boolean>", "", {
+          default: false,
+        })
+        .option("--lowercase <boolean>", "", {
+          default: false,
+        })
+        .action(({ logger, options }) => {
+          let char = "abcdefghijklmnopqestuvwxyz1234567890";
+          if (!options.letters) char = char.replace(/[A-Za-z]/g, "");
+          if (!options.numbers) char = char.replace(/[0-9]/g, "");
+          char = char.split("");
+          let output = "";
+          while (output.length < options.panjang) {
+            let sizeRandom = options.uppercase
+              ? "toUpperCase"
+              : options.lowercase
+              ? "toLowerCase"
+              : Math.floor(Math.random() * 2) === 1
+              ? "toUpperCase"
+              : "toLowerCase";
+            output += char[Math.floor(Math.random() * char.length)][sizeRandom]();
+          }
+          console.log(output);
+        })
 // always run the program at the end
 program.run()
 
