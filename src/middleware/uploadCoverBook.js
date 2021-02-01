3bad7f53c2236bbec5dfb461311ef92aabde2dba
heripurnama <heri.purnama@xapiens.id>
2021-02-02 04:45:16 +0700

    remove debug

--- src/middleware/uploadCoverBook.js
+++ src/middleware/uploadCoverBook.js
@@ -8,8 +8,6 @@ const routers = require("../router/routerAuthors");
 const disk = multer.diskStorage({
   destination: path.join(__dirname, "./../../public/upload/books"),
   filename: (req, file, callback) => {
-    console.log("d", file);
-    console.log("ext", path.extname(file.originalname));
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     callback(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
   },
--- src/middleware/uploadPhoto.js
+++ src/middleware/uploadPhoto.js
@@ -7,8 +7,6 @@ const path = require("path");
 const disk = multer.diskStorage({
   destination: path.join(__dirname, "./../../public/upload/authors"),
   filename: (req, file, callback) => {
-    console.log("d", file);
-    console.log("ext", path.extname(file.originalname));
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     callback(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
   },
